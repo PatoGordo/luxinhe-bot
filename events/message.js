63a83034b4195b3b70acc8f4542988f6acaec7f8
PatoGordo <icariusv@gmail.com>
2021-02-18 14:21:09 +0000

    Agora o bot responde o prefixo quando é mencionado

--- events/message.js
+++ events/message.js
@@ -1,19 +1,29 @@
 const config = require("../config.json")
+const discord = require('discord.js')
+
 module.exports = (client,message) => {
-     if (message.author.bot) return
-     if (message.channel.type == 'dm') return
-     if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return
-     if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return
+	if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)){
+		var randomColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')
+		const messageEmbed = new discord.MessageEmbed()
+			.setColor(randomColor)
+			.setAuthor(config.bot_name, client.user.displayAvatarURL())
+			.setTitle(`Eu respondo pelo prefixo ${config.prefix}`)
+			.setDescription(`Tente usando ${config.prefix}help`)
+		return message.reply(messageEmbed)
+	}
+	if (message.author.bot) return
+	if (message.channel.type == 'dm') return
+	if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return
 
-    const args = message.content
-        .trim().slice(config.prefix.length)
-        .split(/ +/g)
-    const command = args.shift().toLowerCase()
+	const args = message.content
+		.trim().slice(config.prefix.length)
+		.split(/ +/g)
+	const command = args.shift().toLowerCase()
 
-    try {
-        const commandFile = require(`../commands/${command}.js`)
-        commandFile.run(client, message, args)
-    } catch (err) {
-    console.error('Error:' + err)
-  }
+	try {
+		const commandFile = require(`../commands/${command}.js`)
+		commandFile.run(client, message, args)
+	} catch (err) {
+		console.error('Error:' + err)
+	}
 }
